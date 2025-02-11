aaab7294cab866d6c9d3b8aa87cda883cfe776d1
Josue Baldeon Araujo <136621348+Klell11@users.noreply.github.com>
2025-02-11 11:23:42 -0500

    static

--- Aplicaciones/Obras/urls.py
+++ Aplicaciones/Obras/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -33,3 +35,8 @@ urlpatterns = [
    
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
